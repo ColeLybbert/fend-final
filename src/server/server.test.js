5befa3edbc38376606581233636a215e552219a8
Levy Adams <letitslide87@gmail.com>
2022-12-21 15:47:59 -0500

    server.test

---
+++ src/server/server.test.js
@@ -0,0 +1,10 @@
+describe('Test Weather API', function () {
+
+    test('Returns weather for zipcode and time', () => {
+      
+        let returnedData = {};
+        expect(returnedData).toEqual('here we need to see how to make sure the data isnt wrong or undefined');
+    });
+
+    
+});
