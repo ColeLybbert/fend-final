a13c7fbea535c5872bf2ad0c421d10be3d9520cf
Cole Lybbert <cole.lybbert65@gmail.com>
2022-12-21 13:56:18 -0800

    node server changes

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,7 +1,5 @@
 import { weatherAPI } from "../../server/server";
 
-const weatherApiKey = process.env.WEATHER_API_KEY;
-const baseURL = " http://api.weatherbit.io/v2.0/current"
 let button = document.getElementById('submitBtn');
 
 const submitForm = async () => {
