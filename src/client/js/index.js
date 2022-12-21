626ea2502c0cefae8cb7197ee90aadb5133a6086
Cole Lybbert <cole.lybbert65@gmail.com>
2022-12-21 13:41:41 -0800

    node-fetch

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,21 +1,11 @@
+import { weatherAPI } from "../../server/server";
+
 const weatherApiKey = process.env.WEATHER_API_KEY;
 const baseURL = " http://api.weatherbit.io/v2.0/current"
 let button = document.getElementById('submitBtn');
 
 const submitForm = async () => {
     weatherAPI();
-}
-
-export const weatherAPI = async () => {
-    try{
-        const res = await fetch(`${baseURL}?lat=35.7796&lon=-78.6382&key=${weatherApiKey}&include=minutely
-        `);
-        const data = await res.json();
-        console.log(data);
-        return (data);
-    }catch (err) {
-        return `Failed ${err}`
-    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
